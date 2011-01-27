671f877b0b344d5e70d2aaf43ffc47179ab15310
Kahlil Amlani <kamlani@gmail.com>
2011-01-26 18:10:29 -0800

    Tried to add extension

--- rbxmlexport/rbxmlexport/extension.py
+++ rbxmlexport/rbxmlexport/extension.py
@@ -1,9 +1,19 @@
-# Reports extension for Review Board.
-from reviewboard.extensions.base import Extension
+# Reports extension for Review Board.�
+from reviewboard.extensions.base impo�rt Extension
+from reviewboard.extensions.hooks import ReviewRequestActionHook
+
+
+class RBXMLExportActionHook(ReviewRequestActionHook):
+    def get_action_info(self, context):
+       return {'label':'XML Export','uri':'templates/rbxmlexport/test.txt'}
+    
+            
+               
 
 
 class RBXMLExport(Extension):
     is_configurable = False
-
     def __init__(self):
         Extension.__init__(self)
+        self.rr_action_hook = RBXMLExportActionHook(self)
+    
