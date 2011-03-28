5f361348ad6b84ed378ee0fcf50e63bbc7e85d39
Kahlil Amlani <kamlani@gmail.com>
2011-03-27 18:00:18 -0700

    update after review

--- rbxmlexport/rbxmlexport/resources.py
+++ rbxmlexport/rbxmlexport/resources.py
@@ -5,12 +5,13 @@ from reviewboard.reviews.models import ReviewRequest
 from reviewboard.webapi.resources import review_request_resource
 from reviewboard.webapi.resources import WebAPIResource
 from reviewboard.reviews.views import get_file_chunks_in_range
-
+from django.conf import settings
+from django.contrib.sites.models import Site
 
 class ReviewRequestResource(WebAPIResource):
     model = ReviewRequest
-    name = 'ExportXMLRequest'
-    plural_name = 'ExportXMLRequests'
+    name = 'review-request'
+    plural_name = 'review-requests'
     allowed_methods = ('GET')
     uri_object_key = 'id'
     fields = ('id')
@@ -44,7 +45,7 @@ class ReviewRequestResource(WebAPIResource):
 
         return 200, {
             self.item_result_key: {
-                                    'review_request' : NameArray(None,None,review_request_xml),
+                                    'review-request' : NameArray(None,None,review_request_xml),
                                     'reviews ' : NameArray(None,"review",review_xml),
             }
         }
@@ -59,28 +60,33 @@ class ReviewRequestResource(WebAPIResource):
  
         review_array.append(NameArray("user",None,[review.user.username]))
 
-        text_reviews = self.build_text_review(review) #adds text reviews
-        if text_reviews is not None:
-            review_array.append(text_reviews)
+        body_top = self.build_body_top(review) #adds text reviews
+        if body_top is not None:
+            review_array.append(body_top)
 
-        code_reviews = review_array.append(self.build_code_review(request, review)) #adds code reviews
-        if code_reviews is not None:
-            review_array.append(code_reviews)
+   
+        body_bottom = []
+        body_bottom_code = self.build_body_top_diffs(request, review) #adds code reviews
+        body_bottom_screenshot = self.build_body_top_screenshot(review) #adds screenshot reviews
 
-        screenshots_reviews = self.build_screenshot_review(review) #adds screenshot reviews
-        if screenshots_reviews is not None:
-            review_array.append(screenshots_reviews)
-       
+        if body_bottom_code is not None: body_bottom.extend(body_bottom_code)
+        if body_bottom_screenshot is not None: body_bottom.extend(body_bottom_screenshot)
+        if len(body_bottom): review_array.append(NameArray("comments",None,body_bottom))
+      
         if len(review_array):
             return NameArray(None,None,review_array)
+
         return None
 
 
-    def build_text_review(self, review):
+    def build_body_top(self, review):
 
         text_review_array = []
         if review.body_top is not u'':
-            text_review_array.append(NameArray(None,"text",[review.body_top]))
+            if review.body_top == "Ship It!": 
+                return(NameArray("ship-it",None,["1"]))
+            else: 
+                text_review_array.append(NameArray(None,"text",[review.body_top]))
 
         review_replies = []
         for reply in review.public_replies():
@@ -93,75 +99,87 @@ class ReviewRequestResource(WebAPIResource):
             text_review_array.append(NameArray("replies","reply",review_replies)) 
 
         if len(text_review_array):
-            return NameArray("text_review",None,text_review_array)
+            return NameArray("body-top",None,text_review_array)
 
         return None
 
 
-    def build_screenshot_review(self, review):       
+    def build_body_top_screenshot(self, review):       
         screenshot_review_array = []
 
         for screenshot in review.screenshot_comments.all():        
 
             screenshot_node = [] # Write SS Info
-            screenshot_node.append(NameArray("url",None,[screenshot.get_image_url()]))
+            screenshot_node.append(NameArray("type",None,["screenshot"]))
+            screenshot_node.append(NameArray("url",None,["http://"+Site.objects.get_current().domain+screenshot.get_image_url()]))
             screenshot_node.append(NameArray("text",None,[screenshot.text]))
 
             screenshot_replies = self.get_reply_comment(screenshot.public_replies()) # get replies      
             if len(screenshot_replies):
                 screenshot_node.append(NameArray("replies","reply",screenshot_replies))
 
-            screenshot_review_array.append(NameArray("screenshot_review",None,screenshot_node))
+            screenshot_review_array.append(NameArray("comment",None,screenshot_node))
         
         if len(screenshot_review_array):
-            return NameArray("screenshot_reviews",None,screenshot_review_array)
+            return screenshot_review_array
         return None
 
 
-    def build_code_review(self, request,review):
-        code_review_array = []
+    def build_body_top_diffs(self, request,review):
+        diff_review_array = []
 
         context = RequestContext(request, {
                     'review_request': request,
                     'review': review,
                     })
 
-        code_comments = review.comments.all()
-        for code_comment in code_comments:
+        diff_comments = review.comments.all()
+        for diff_comment in diff_comments:
 
             content = list(get_file_chunks_in_range(context,
-                            code_comment.filediff,
-                            code_comment.interfilediff,
-                            code_comment.first_line,
-                            code_comment.num_lines,
+                            diff_comment.filediff,
+                            diff_comment.interfilediff,
+                            diff_comment.first_line,
+                            diff_comment.num_lines,
                             False)) 
 
-            code_node = [] #write code-review info                                          
-            code_node.append(NameArray("text",None,[code_comment.text]))
-            code_node.append(NameArray("type",None,[content[0]['change']]))
-            code_node.append(NameArray("file",None,[code_comment.filediff.source_file]))
-            code_node.append(NameArray("before",None,[content[0]['lines'][0][2]]))
-            code_node.append(NameArray("after",None,[content[0]['lines'][0][5]]))
-       
-            code_replies = self.get_reply_comment(code_comment.public_replies()) # get replies                 
-            if len(code_replies):
-                code_node.append(NameArray("replies","reply",code_replies))
-        
-            code_review_array.append(NameArray("code_review",None,code_node))
+            diff_node = [] #write code-review info                                          
+            diff_node.append(NameArray("type",None,[content]))
+            diff_node.append(NameArray("text",None,[diff_comment.text]))
+            diff_node.append(NameArray("file",None,[diff_comment.filediff.source_file]))
+            
+            chunks = []
+            counter = 0
+            for single_line in content[0]['lines']:
+                chunk = []
+                chunk.append(NameArray("linenum",None,[single_line[0]]))
+                chunk.append(NameArray("before",None,[single_line[2]]))
+                chunk.append(NameArray("after",None,[single_line[5]]))
+                counter = counter + 1
+                chunks.append(NameArray("line",None,chunk))
 
-        if  len(code_review_array):
-            return NameArray("code_reviews",None,code_review_array)
+            diff_node.append(NameArray("chunk",None,chunks))
+        
+
+       
+            diff_replies = self.get_reply_comment(diff_comment.public_replies()) # get replies                 
+            if len(diff_replies):
+                diff_node.append(NameArray("replies","reply",diff_replies))
+        
+            diff_review_array.append(NameArray("comment",None,diff_node))
+
+        if  len(diff_review_array):
+            return diff_review_array
 
         return None
-
 
     def get_reply_comment(self, public_replies):
         replies = []
         for reply in public_replies:
-            inner_code_reply = []
-            inner_code_reply.append(NameArray(None,"user",[reply.review.get().user.username]))
-            inner_code_reply.append(NameArray(None,"comment",[reply.text]))
-            replies.append(NameArray(None,None,inner_code_reply))
+            reply_array = []
+            reply_array.append(NameArray(None,"user",[reply.review.get().user.username]))
+            reply_array.append(NameArray(None,"comment",[reply.text]))
+            replies.append(NameArray(None,None,reply_array))
         return replies
 
 reviewing_session_resource = ReviewRequestResource()
